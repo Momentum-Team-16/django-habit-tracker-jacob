9b1d59a6484070cd01c60478326c45f372c1dc23
Jacob Gilbert <jacobgilb03@gmail.com>
2023-01-18 09:16:12 -0500

    Deploy

---
+++ core/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import User, Habit, Record
+
+# Register your models here.
+
+admin.site.register(User)
+admin.site.register(Habit)
+admin.site.register(Record)
---
+++ core/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+class User(AbstractUser):
+    pass
+
+class Habit(models.Model):
+    name = models.CharField(max_length=250)
+    metric = models.PositiveIntegerField(null=True, blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    unit_of_measure = models.CharField(max_length=255)
+    user = models.ForeignKey('User', on_delete=models.CASCADE, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+class Record(models.Model):
+    habit = models.ForeignKey('Habit', on_delete=models.CASCADE, related_name='records')
+    finished_date = models.DateField(blank=True, null=True)
+    amount = models.PositiveIntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return f"Record for {self.habit.name}"
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['habits', 'finished_date'], name='unique_daily_record')
+        ]
